623102d01b385b55c9dc0a0f59d071c2d29d279e
Petro Psekho <psekho.pp@gmail.com>
2025-04-17 17:12:17 +0300

    add modal window

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Можливе місце для інтеграції оплати або аналітики
+document
+  .querySelector('.btn-primary[href="#"]')
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("modal").style.display = "flex";
+  });
+
+document.querySelector(".close-btn").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "none";
+});
+
+// Закриття модалки по кліку за межами
+window.addEventListener("click", function (e) {
+  const modal = document.getElementById("modal");
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
